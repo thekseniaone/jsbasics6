ea401b5902e4ad1d204a1a5d6587caba5a965df5
Ksenia <neverknowme@bk.ru>
2023-10-20 17:42:14 +0300

    adding jsdoc

--- src/app.js
+++ src/app.js
@@ -1,15 +1,26 @@
+/**
+ * Объект с набором имен студентов и оценок
+ * @type {{ Anna: number; Ivan: number; Elena: number; Arseniy: number; }}
+ */
+
 const journal = {
-    Anna : 4,
-    Ivan : 3,
-    Elena : 5,
-    Arseniy : 5,
+    Anna: 4,
+    Ivan: 3,
+    Elena: 5,
+    Arseniy: 5,
 }
 
 
-function getScore(){
+/**
+ * Функция с подсчетом оценок
+ * @param {*} journal
+ * @returns {number}
+ */
+
+function getScore(journal) {
     let score = 0;
-    for (let key in journal){
-       score += journal[key];
+    for (let key in journal) {
+        score += journal[key];
     }
     return score;
 }
