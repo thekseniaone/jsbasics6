6d5c777219f41f5e4cf837b26e5555843486fb14
Ksenia <neverknowme@bk.ru>
2023-10-20 17:22:22 +0300

    adding the app

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-// init
+const journal = {
+    Anna : 4,
+    Ivan : 3,
+    Elena : 5,
+    Arseniy : 5,
+}
+
+
+function getScore(){
+    let score = 0;
+    for (let key in journal){
+       score += journal[key];
+    }
+    return score;
+}
+
+console.log(getScore(journal));
+
